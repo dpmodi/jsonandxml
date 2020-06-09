508fca521b026c1c4c75133545f212e6e883b31c
Devang <dmodi@roku.com>
2020-06-09 13:47:24 -0700

    Initial commit

---
+++ src/main/java/com/dmodi/jsonandxml/JsonandxmlApplication.java
@@ -0,0 +1,13 @@
+package com.dmodi.jsonandxml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonandxmlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JsonandxmlApplication.class, args);
+  }
+
+}
